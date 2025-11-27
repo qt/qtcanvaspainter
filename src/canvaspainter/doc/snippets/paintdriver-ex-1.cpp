7c8bee4a6144d9cbd58b58e622214bc22b8c1620
Laszlo Agocs <laszlo.agocs@qt.io>
2025-11-27 12:08:08 +0100

    Add initial docs for factory and paintdriver
    Change-Id: Idca6e4743f3088c7af2b3d01445c7cf863c66395
    Reviewed-by: Kaj Grönholm <kaj.gronholm@qt.io>

---
+++ src/canvaspainter/doc/snippets/paintdriver-ex-1.cpp
@@ -0,0 +1,62 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+//![0]
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    // std::unique_ptr<QRhi> rhi(QRhi::create(...));
+
+    std::unique_ptr<QRhiTexture> tex(rhi->newTexture(QRhiTexture::RGBA8, QSize(1280, 720), 1,
+                                     QRhiTexture::RenderTarget | QRhiTexture::UsedAsTransferSource));
+    tex->create();
+    std::unique_ptr<QRhiRenderBuffer> ds(rhi->newRenderBuffer(QRhiRenderBuffer::DepthStencil, QSize(1280, 720)));
+    ds->create();
+    QRhiTextureRenderTargetDescription rtDesc;
+    rtDesc.setColorAttachments({ tex.get() });
+    rtDesc.setDepthStencilBuffer(ds.get());
+    std::unique_ptr<QRhiTextureRenderTarget> rt(rhi->newTextureRenderTarget(rtDesc));
+    std::unique_ptr<QRhiRenderPassDescriptor> rp(rt->newCompatibleRenderPassDescriptor());
+    rt->setRenderPassDescriptor(rp.get());
+    rt->create();
+
+    std::unique_ptr<QCPainterFactory> factory(new QCPainterFactory);
+    QCPainter *painter = factory->create(rhi.get());
+    QCRhiPaintDriver *pd = factory->paintDriver();
+
+    QRhiCommandBuffer *cb;
+    QRhiReadbackResult readbackResult;
+
+    rhi->beginOffscreenFrame(&cb);
+    pd->resetForNewFrame();
+
+    {
+        pd->beginPaint(cb, rt.get());
+
+        painter->beginPath();
+        painter->circle(640, 360, 180);
+        painter->setFillStyle(Qt::red);
+        painter->fill();
+
+        pd->endPaint();
+
+        QRhiResourceUpdateBatch *u = rhi->nextResourceUpdateBatch();
+        u->readBackTexture({ tex.get() }, &readbackResult);
+        cb->resourceUpdate(u);
+    }
+
+    rhi->endOffscreenFrame();
+
+    QImage image(reinterpret_cast<const uchar *>(readbackResult.data.constData()),
+                    readbackResult.pixelSize.width(),
+                    readbackResult.pixelSize.height(),
+                    QImage::Format_RGBA8888);
+    if (rhi->isYUpInFramebuffer())
+        image.flip();
+
+    image.save("result.png");
+
+    return 0;
+}
+//![0]
--- src/canvaspainter/qcpainterfactory.cpp
+++ src/canvaspainter/qcpainterfactory.cpp
@@ -18,15 +18,31 @@ QT_BEGIN_NAMESPACE
     \inmodule QtCanvasPainter
 
     Applications rendering via QCPainter do not create instances of the painter
-    class themselves, but rather are expected to use a QCPainterFactory. This is
-    relevant only when working with QCPainter without a convenience class such
-    as QCPainterWidget or QQuickCPainterItem, because those provide a QCPainter
-    instance to the application.
+    class themselves, but rather are expected to use a QCPainterFactory.
 
-    All drawing code that operates on the same thread and is using the same QRhi
-    underneath is recommended to share and reuse the same QCPainter, instead of
-    having a dedicated painter (and so factory) in each component. This can be achieved
-    by calling sharedInstance(), instead of constructing a new QCPainterFactory.
+    \note This class is relevant only when working with QCPainter \b without a
+    convenience class such as QCPainterWidget or QQuickCPainterItem, because
+    those provide a QCPainter instance to the application.
+
+    The following is an example code snippet of getting a QCPainter for use with
+    an existing, successfully initialized QRhi:
+
+    \code
+        std::unique_ptr<QCPainterFactory> factory(new QCPainterFactory);
+        QCPainter *painter = factory->create(rhi);
+        QCRhiPaintDriver *paintDriver = factory->paintDriver();
+    \endcode
+
+    All drawing code that operates on the same thread with the same QRhi is
+    recommended to share and reuse the same QCPainter, instead of having a
+    dedicated painter (and so factory) in each component. This can be achieved
+    by calling sharedInstance(), instead of constructing a new QCPainterFactory:
+
+    \code
+        QCPainterFactory *factory = QCPainterFactory::sharedInstance(rhi);
+        QCPainter *painter = factory->painter();
+        QCRhiPaintDriver *paintDriver = factory->paintDriver();
+    \endcode
  */
 
 struct QCPainterFactoryGlobal
@@ -36,7 +52,26 @@ struct QCPainterFactoryGlobal
 
 Q_GLOBAL_STATIC(QCPainterFactoryGlobal, qcpd_g);
 
-// what's returned from here needs no create(), although it is nor harmful either to call it
+/*!
+    \return a QCPainterFactory instance associated with the given \a rhi.
+
+    Calling this function with the same \a rhi will always return the same
+    QCPainterFactory.
+
+    There is no need to call create() on the returned factory, and the \l
+    painter() can be queried right away, because the result is always already
+    initialized, meaning isValid() returns true.
+
+    This function is thread safe. However, the QRhi threading rules apply as
+    usual: a QRhi object, and by extension, an instance of QCPainterFactory,
+    QCRhiPaintDriver, and QCPainter associated with that QRhi are all expected
+    to be used on the same one single thread.
+
+    \note The returned QCPainterFactory is not owned by the caller, and it
+    becomes invalid when \a rhi is destroyed.
+
+    \sa painter()
+ */
 QCPainterFactory *QCPainterFactory::sharedInstance(QRhi *rhi)
 {
     static QMutex m;
@@ -64,32 +99,71 @@ QCPainterFactory *QCPainterFactory::sharedInstance(QRhi *rhi)
     return pd;
 }
 
+/*!
+    Constructor.
+
+    \sa sharedInstance()
+ */
 QCPainterFactory::QCPainterFactory()
     : d(new QCPainterFactoryPrivate)
 {
 }
 
+/*
+    Destructor.
+ */
 QCPainterFactory::~QCPainterFactory()
 {
     destroy();
     delete d;
 }
 
+/*!
+    \return true if the create() has ben called successfully.
+ */
 bool QCPainterFactory::isValid() const
 {
     return d->painter && d->renderer.isValid();
 }
 
+/*!
+    \return the painter, or null if isValid() is false.
+
+    \note The returned object is not owned by the caller.
+ */
 QCPainter *QCPainterFactory::painter()
 {
     return d->painter.get();
 }
 
+/*!
+    \return the paint driver object, or null if isValid() is false.
+
+    \note The returned object is not owned by the caller.
+ */
 QCRhiPaintDriver *QCPainterFactory::paintDriver()
 {
     return d->paintDriver.get();
 }
 
+/*!
+    Initializes the QCPainter rendering infrastructure, if it has not been done
+    already for this factory.
+
+    \return the painter, or null if the renderer could not be initialized.
+
+    The factory will be associated with \a rhi, and it cannot be used with any
+    other QRhi, unless destroy(), and then create() are called again.
+
+    Repeated calls to this function have no effect and are harmless. Call
+    destroy() first if a reinitialization of the painter infrastructure is
+    desired, possibly with a different \a rhi.
+
+    \note There is no need to call this function when sharedInstance() was used
+    to retrieve a factory instance.
+
+    \sa destroy(), isValid(), painter()
+ */
 QCPainter *QCPainterFactory::create(QRhi *rhi)
 {
     if (!d->paintDriver)
@@ -104,6 +178,11 @@ QCPainter *QCPainterFactory::create(QRhi *rhi)
         d->renderer.create(rhi, d->painter.get());
     }
 
+    if (!d->renderer.isValid()) {
+        qWarning("QCPainterFactory::create() failed to initialize renderer");
+        return nullptr;
+    }
+
     QCRhiPaintDriverPrivate *pd = QCRhiPaintDriverPrivate::get(d->paintDriver.get());
     pd->painter = d->painter.get();
     pd->renderer = &d->renderer;
@@ -113,6 +192,13 @@ QCPainter *QCPainterFactory::create(QRhi *rhi)
     return d->painter.get();
 }
 
+/*!
+    Tears down the rendering infrastructure. Normally there is no need to call
+    this function. Rather, it is used in situations where it will be followed by
+    a create().
+
+    \sa create()
+ */
 void QCPainterFactory::destroy()
 {
     if (d->painter)
