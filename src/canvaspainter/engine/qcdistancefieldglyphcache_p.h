087a238016fa0102d8d253715d8d129eb33610cd
Paul Olav Tvete <paul.tvete@qt.io>
2025-12-11 10:46:18 +0100

    Add font size to glyph cache key
    This fixes a bug with drawing the same text at different font sizes.
    
    Task-number: QTBUG-140221
    Pick-to: 6.11
    Change-Id: Id2d11a40b7ac5d992a60815eb0ebe675024d1fc8
    Reviewed-by: Kaj Grönholm <kaj.gronholm@qt.io>

--- src/canvaspainter/engine/qcdistancefieldglyphcache_p.h
+++ src/canvaspainter/engine/qcdistancefieldglyphcache_p.h
@@ -51,6 +51,7 @@ public:
         QString text;
         QCPainter::TextAlign textAlign;
         QCPainter::WrapMode wrapMode;
+        float pixelSize;
         float lineWidth;
         float letterSpacing;
         float wordSpacing;
@@ -112,7 +113,8 @@ inline size_t qHash(const QCDistanceFieldGlyphCache::FontKey &f, size_t seed = 0
 inline bool operator==(
     const QCDistanceFieldGlyphCache::GlyphCacheKey &a, const QCDistanceFieldGlyphCache::GlyphCacheKey &b)
 {
-    return a.fontKey == b.fontKey && a.text == b.text && a.textAlign == b.textAlign
+    return a.fontKey == b.fontKey && qFuzzyCompare(a.pixelSize, b.pixelSize)
+           && a.text == b.text && a.textAlign == b.textAlign
            && a.wrapMode == b.wrapMode && qFuzzyCompare(a.lineWidth, b.lineWidth)
            && qFuzzyCompare(a.letterSpacing, b.letterSpacing)
            && qFuzzyCompare(a.wordSpacing, b.wordSpacing);
@@ -127,7 +129,7 @@ inline bool operator!=(
 inline size_t qHash(const QCDistanceFieldGlyphCache::GlyphCacheKey &k, size_t seed = 0)
 {
     return qHashMulti(seed, k.fontKey, k.text, k.textAlign, k.wrapMode, k.lineWidth,
-                      k.letterSpacing, k.wordSpacing);
+                      k.letterSpacing, k.wordSpacing, k.pixelSize);
 }
 
 QT_END_NAMESPACE
