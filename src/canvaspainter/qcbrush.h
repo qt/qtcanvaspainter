eecf34c54472fc586141d8279bb76bc8d4839fd1
Laszlo Agocs <laszlo.agocs@qt.io>
2026-02-06 12:04:53 +0100

    Add move ctor to QCBrush
    Match QCImage and QCOffscreenCanvas.
    
    Using noexcept for consistency. If that is not wanted, the noexcept
    should be removed everywhere in a separate follow-up patch.
    
    Pick-to: 6.11
    Change-Id: I9ee18d31b304bd51fb759c82c42b8e7f8d518758
    Reviewed-by: Kaj Grönholm <kaj.gronholm@qt.io>

--- src/canvaspainter/qcbrush.h
+++ src/canvaspainter/qcbrush.h
@@ -35,6 +35,7 @@ public:
     ~QCBrush();
 
     QCBrush &operator=(const QCBrush &brush) noexcept;
+    QCBrush(QCBrush &&other) noexcept = default;
     QT_MOVE_ASSIGNMENT_OPERATOR_IMPL_VIA_PURE_SWAP(QCBrush)
     void swap(QCBrush &other) noexcept { baseData.swap(other.baseData); }
 
