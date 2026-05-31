2420076ff2fcd7f4311a756297ae1180d2c6c40e
Jerome Pasion <jerome.pasion@qt.io>
2026-05-31 15:00:14 +0200

    Doc: Add alternate text for Qt Canvas Painter drawImage images
    Alternate text (alt text) improves the documentation experience for
    screen readers and for other accessibility tools.
    
    Task-number: QTBUG-135098
    Change-Id: I309f0b7647d8e27a2ea2d81f56d9f2663a6352fb
    Reviewed-by: David Boddie <david.boddie@qt.io>
    (cherry picked from commit 641e451aa01a633cf2d2cde952dc53490dfe67ba)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 8c28429d7582597508f3f846b20ffebcf862bcaa)

--- src/canvaspainter/qcanvaspainter.cpp
+++ src/canvaspainter/qcanvaspainter.cpp
@@ -1992,6 +1992,7 @@ void QCanvasPainter::drawBoxShadow(const QCanvasBoxShadow &shadow)
     \table
     \row
     \li \inlineimage qcpainter-drawimage.webp
+        {Green Qt logo drawn at its default size}
     \li
     \code
     static QImage logo(":/qt_logo.png");
@@ -2020,6 +2021,7 @@ void QCanvasPainter::drawImage(const QCanvasImage &image, float x, float y)
     \table
     \row
     \li \inlineimage qcpainter-drawimage2.webp
+        {Green Qt logo stretched taller to fit a custom width and height}
     \li
     \code
     static QImage logo(":/qt_logo.png");
@@ -2056,6 +2058,7 @@ void QCanvasPainter::drawImage(const QCanvasImage &image, float x, float y, floa
     \table
     \row
     \li \inlineimage qcpainter-drawimage3.webp
+        {Enlarged Q letter cropped from a source region of the Qt logo}
     \li
     \code
     static QImage logo(":/qt_logo.png");
