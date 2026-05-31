a697d5d4ef17495529e684531d604947c0dcdd52
Jerome Pasion <jerome.pasion@qt.io>
2026-05-31 15:00:14 +0200

    Doc: Add alternate text for Qt Canvas Painter global compositing and antialiasing images
    Alternate text (alt text) improves the documentation experience for
    screen readers and for other accessibility tools.
    
    Task-number: QTBUG-135098
    Change-Id: Ifcc8b8eb5b808ebb472264014b7c9aaec0268d44
    Reviewed-by: David Boddie <david.boddie@qt.io>
    (cherry picked from commit 52d971b283943c7b085cad86cce91a0fa6821d29)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 53165faca0d7d028b69c80fde5d14531e05e1759)

--- src/canvaspainter/qcanvaspainter.cpp
+++ src/canvaspainter/qcanvaspainter.cpp
@@ -720,6 +720,8 @@ void QCanvasPainter::setLineJoin(LineJoin join)
     \table
     \row
     \li \inlineimage qcpainter-globalalpha.webp
+        {Four Qt logos on green tiles fading to fully transparent across the
+        grid}
     \li
     \code
     static QImage logo(":/qt_logo2.png");
@@ -764,6 +766,8 @@ void QCanvasPainter::setGlobalCompositeOperation(CompositeOperation operation)
     \table
     \row
     \li \inlineimage qcpainter-globalbrightness.webp
+        {Four Qt logos on green tiles darkening from bright to nearly black
+        across the grid}
     \li
     \code
     static QImage logo(":/qt_logo2.png");
@@ -796,6 +800,8 @@ void QCanvasPainter::setGlobalBrightness(float value)
     \table
     \row
     \li \inlineimage qcpainter-globalcontrast.webp
+        {Four Qt logos on green tiles ranging from high contrast to washed-out
+        gray}
     \li
     \code
     static QImage logo(":/qt_logo2.png");
@@ -828,6 +834,8 @@ void QCanvasPainter::setGlobalContrast(float value)
     \table
     \row
     \li \inlineimage qcpainter-globalsaturate.webp
+        {Four Qt logos on green tiles fading from vivid color to grayscale
+        across the grid}
     \li
     \code
     static QImage logo(":/qt_logo2.png");
@@ -2348,6 +2356,8 @@ QRectF QCanvasPainter::textBoundingBox(const QString &text, const QRectF &rect)
     \table
     \row
     \li \inlineimage qcpainter-antialias.webp
+        {Stack of wavy lines growing progressively softer and blurrier with
+        increasing antialiasing}
     \li
     \code
     p->setLineWidth(6);
