13697985190d54330f5cc7d114199b05e3eac072
Laszlo Agocs <laszlo.agocs@qt.io>
2026-06-25 10:39:43 +0200

    Fix "dangling" resource update batch when nothing is drawn
    Change-Id: Ia570ac7d435c5cbbfb26524876ccfaa734d777e0
    Reviewed-by: Kaj Grönholm <kaj.gronholm@qt.io>
    (cherry picked from commit c7328ba0e63325b3913d72de9d562f15ec9ca4a5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/canvaspainter/engine/qcpainterrhirenderer.cpp
+++ src/canvaspainter/engine/qcpainterrhirenderer.cpp
@@ -2713,6 +2713,12 @@ void QCPainterRhiRenderer::endPrepare()
             }
         }
     }
+
+    // In case there are no draw calls, but something enqueued resource
+    // operations, such as a texture upload.
+    if (rhiCtx->resourceUpdates)
+        commitResourceUpdates();
+
 #ifdef QCPAINTER_PERF_DEBUG
     m_e->perf.tick();
 #endif
